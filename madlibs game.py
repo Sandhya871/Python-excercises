c706a0c8a471d99d0318b5aae51fbefe4677992c
Sandiyadevi.v <sandhyaqv685@gmail.com>
2026-06-12 17:50:17 +0530

    madlibs game.py

---
+++ madlibs game.py
@@ -0,0 +1,24 @@
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person,place,thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit ,I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+
+
+# OUTPUT 
+'''The program creates and displays a customized story by inserting the user's inputs into predefined sentences.'''
+Enter an adjective (description): suspicious
+Enter a noun (person,place,thing): Angel  
+Enter an adjective (description): angry
+Enter a verb ending with 'ing'screeching
+Enter an adjective (description): happy
+
+Today I went to a suspicious zoo.
+In an exhibit ,I saw a Angel
+Angel was angry and screeching
+I was happy!
